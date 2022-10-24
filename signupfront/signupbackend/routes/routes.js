bd909b393cc18f6399ef95776f9a3b5e3d77fef5
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-10-25 00:04:22 +0200

    updated

--- signupfront/signupbackend/routes/routes.js
+++ signupfront/signupbackend/routes/routes.js
@@ -1,9 +1,25 @@
-const { request, response } = require('express');
-const express = require('express');
-const router =  express.Router()
+ //const { request, response } = require('express');
+const express = require("express");
+const router = express.Router();
+const SignUpTemplateCopy = require("../models/SignUpModels");
 
-router.post("/signup",(request,response)=>{
-    response.send("send")
-})
+router.post("/signup", (request, response) => {
+  const signedUpUser = new SignUpTemplateCopy({
+    fullName: request.body.fullName,
+    username: request.body.username,
+    email: request.body.email,
+    password: request.body.password,
+  });
+  signedUpUser
+    .save()
+    .then((data) => {
+      response.json(data);
+    })
+    .catch((error) => {
+      response.json(error);
+    });
+});
 
-module.exports = router
+
+
+module.exports = router;
